df231229196c087d0c42aa59fa7c916da3a6d199
uvajuha <uvajuhaonline@mail.ru>
2024-12-18 23:39:33 +0300

    feat: Первая, пока провальна попытка изначальной миграции

---
+++ DBConnect/ApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using DBConnect.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DBConnect
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<NewsInfoModel> News => Set<NewsInfoModel>();
+        public ApplicationContext()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=D:\\SQLLiteDB\\webApp.db");
+        }
+    }
+}
